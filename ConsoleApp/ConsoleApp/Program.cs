69f1e75f034aea2b0063b81f9bb73bc4882b6d81
Cheng-Jhe <chegnjheli@chipbond.com.tw>
2019-10-03 15:47:34 +0800

    add ex
    Signed-off-by: Cheng-Jhe <chegnjheli@chipbond.com.tw>

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,9 +16,150 @@ namespace ConsoleApp
         {
             Console.WriteLine("Hello World!");
         }
-        public void Publisher()
+
+        public void DirectPublisher()
+        {
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost", RequestedHeartbeat = 60, AutomaticRecoveryEnabled = true };
+            using (IConnection connection = factory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "queue-1", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                    channel.QueueDeclare(queue: "queue-2", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    channel.ExchangeDeclare(exchange: "queue-direct", type: ExchangeType.Direct, durable: true, autoDelete: false);
+
+                    channel.QueueBind(queue: "queue-1", exchange: "queue.direct", routingKey: "xml");
+                    channel.QueueBind(queue: "queue-2", exchange: "queue.direct", routingKey: "json");
+
+                    string message = "Hello World";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "queue.direct",
+                                         routingKey: "xml",
+                                         basicProperties: null,
+                                         body: body);
+
+                    channel.BasicPublish(exchange: "queue.direct",
+                                routingKey: "json",
+                                basicProperties: null,
+                                body: body);
+                }
+            }
+
+        }
+
+        public void TopicPublisher()
         {
             ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "queue-1", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                    channel.QueueDeclare(queue: "queue-2", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    channel.ExchangeDeclare(exchange: "queue-topic", type: ExchangeType.Topic, durable: true, autoDelete: false);
+
+                    channel.QueueBind(queue: "queue-1", exchange: "queue.topict", routingKey: "*.json");
+                    channel.QueueBind(queue: "queue-2", exchange: "queue.topic", routingKey: "#.json");
+
+                    string message = "Hello World";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "queue.topic",
+                                         routingKey: "data.json",
+                                         basicProperties: null,
+                                         body: body);
+
+                    channel.BasicPublish(exchange: "queue.topic",
+                                routingKey: "my.data.json",
+                                basicProperties: null,
+                                body: body);
+                }
+            }
+
+        }
+
+
+        public void FanoutPublisher()
+        {
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "queue-1", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                    channel.QueueDeclare(queue: "queue-2", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    channel.ExchangeDeclare(exchange: "queue-fanout", type: ExchangeType.Fanout, durable: true, autoDelete: false);
+
+                    channel.QueueBind(queue: "queue-1", exchange: "queue.fanout", routingKey: null);
+                    channel.QueueBind(queue: "queue-2", exchange: "queue.fanout", routingKey: null);
+
+                    string message = "Hello World";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "queue.fanout",
+                                         routingKey: null,
+                                         basicProperties: null,
+                                         body: body);
+                }
+            }
+
+        }
+
+        public void HeaderPublisher()
+        {
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "queue-1", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                    channel.QueueDeclare(queue: "queue-2", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    channel.ExchangeDeclare(exchange: "queue-header", type: ExchangeType.Headers, durable: true, autoDelete: false);
+
+                    Dictionary<string, object> xmlHeader = new Dictionary<string, object>{
+                        { "format", "xml" },
+                        { "type", "report" },
+                        { "x-match", "all" }
+                    };
+                    channel.QueueBind(queue: "queue-1", exchange: "queue.header", routingKey: null, arguments: xmlHeader);
+
+                    Dictionary<string, object> jsonHeader = new Dictionary<string, object> {
+                        { "format", "json" },
+                        { "type", "report" },
+                        { "x-match", "any" }
+                    };
+                    channel.QueueBind(queue: "queue-2", exchange: "queue.header", routingKey: null, arguments: jsonHeader);
+
+                    string message = "Hello World";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+
+                    Dictionary<string, object> myHeader = new Dictionary<string, object>
+                    {
+                        { "type", "report" }
+                    };
+
+                    IBasicProperties properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+                    properties.Headers = myHeader;
+                    channel.BasicPublish(exchange: "queue.header",
+                                         routingKey: null,
+                                         basicProperties: properties,
+                                         body: body);
+                }
+            }
+
+        }
+
+
+        public void Publisher()
+        {
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost", RequestedHeartbeat = 60, AutomaticRecoveryEnabled = true };
             using (IConnection connection = factory.CreateConnection())
             {
                 using (IModel channel = connection.CreateModel())
@@ -41,7 +183,7 @@ namespace ConsoleApp
         public void Receiver()
         {
 
-            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost", DispatchConsumersAsync = true };
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost", RequestedHeartbeat = 60, AutomaticRecoveryEnabled = true };
             IConnection connection = factory.CreateConnection();
             IModel channel = connection.CreateModel();
 
@@ -53,18 +195,18 @@ namespace ConsoleApp
                byte[] body = e.Body;
                string message = Encoding.UTF8.GetString(body);
                Console.WriteLine($"Received:{message}");
-                //channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
-                //channel.BasicNack(deliveryTag:e.DeliveryTag, multiple:false, requeue:true);
-            };
+               //channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+               //channel.BasicNack(deliveryTag:e.DeliveryTag, multiple:false, requeue:true);
+           };
             channel.BasicConsume(queue: "test-queue", autoAck: true, consumer: consumer);
 
         }
         public void ReceiverAsync()
         {
-            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost", DispatchConsumersAsync = true };
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost", DispatchConsumersAsync = true, RequestedHeartbeat = 60, AutomaticRecoveryEnabled = true };
             IConnection connection = factory.CreateConnection();
             IModel channel = connection.CreateModel();
-
+            channel.BasicQos(prefetchCount: 1, prefetchSize: 1, global: true);
             channel.QueueDeclare(queue: "test-queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
             AsyncEventingBasicConsumer consumer = new AsyncEventingBasicConsumer(channel);
@@ -74,9 +216,9 @@ namespace ConsoleApp
                 string message = Encoding.UTF8.GetString(body);
                 await Task.Delay(5000);
                 Console.WriteLine($"Received:{message}");
-                    //channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
-                    //channel.BasicNack(deliveryTag:e.DeliveryTag, multiple:false, requeue:true);
-                };
+                //channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+                //channel.BasicNack(deliveryTag:e.DeliveryTag, multiple:false, requeue:true);
+            };
             channel.BasicConsume(queue: "test-queue", autoAck: true, consumer: consumer);
 
 
